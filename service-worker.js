3ca7589a3587aa5db1aefaa9ddc0fe0d5c9e83e9
thefuture845-svg <thefuture845@gmail.com>
2026-09-14 20:25:17 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE = 'gestor-maquinas-v2';
+const CACHE = "gestor-maquinas-v3";
 
 const FILES = [
-  './',
-  './index.html',
-  './manifest.webmanifest'
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
@@ -14,7 +14,7 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
@@ -28,7 +28,7 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
